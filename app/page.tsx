6debe34f146914ee020460950d1695669549641b
mohamed zouig <zouig.mohamed@gmail.com>
2024-11-12 14:06:20 +0100

    eslint & prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-violet-700 font-bold">
+    <h1 className="text-3xl font-bold text-violet-700">
       welcome to the world of nextjs
     </h1>
   );
