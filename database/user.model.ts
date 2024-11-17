d92686a75fd6c3a34df8b67d662cf2fbc77c95df
mohamed zouig <zouig.mohamed@gmail.com>
2024-11-17 19:43:35 +0100

    create user model

---
+++ database/user.model.ts
@@ -0,0 +1,51 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IUser{
+    name: string;
+    username: string;
+    email: string;
+    bio?: string;
+    image: string;
+    location?: string;
+    portfolio?: string;
+    reputation?:number
+}
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    bio: {
+      type: String,
+    },
+    image: {
+      type: String,
+      unique: true,
+    },
+    location: {
+      type: String,
+    },
+    portfolio: {
+      type: String,
+    },
+    reputation: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = models?.user || model<IUser>("User", UserSchema);
+export default User;
