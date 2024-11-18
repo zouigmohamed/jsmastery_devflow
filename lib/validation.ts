8835f0a6b9c8ac49578f77aa7607d4b5192bf8df
mohamed zouig <zouig.mohamed@gmail.com>
2024-11-18 18:44:54 +0100

    implement create and read account routes

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,14 +1,17 @@
 import { z } from "zod";
+
 export const SignInSchema = z.object({
   email: z
     .string()
     .min(1, { message: "Email is required" })
     .email({ message: "Please provide a valid email address." }),
+
   password: z
     .string()
     .min(6, { message: "Password must be at least 6 characters long. " })
     .max(100, { message: "Password cannot exceed 100 characters." }),
 });
+
 export const SignUpSchema = z.object({
   username: z
     .string()
@@ -17,6 +20,7 @@ export const SignUpSchema = z.object({
     .regex(/^[a-zA-Z0-9_]+$/, {
       message: "Username can only contain letters, numbers, and underscores.",
     }),
+
   name: z
     .string()
     .min(1, { message: "Name is required." })
@@ -24,10 +28,12 @@ export const SignUpSchema = z.object({
     .regex(/^[a-zA-Z\s]+$/, {
       message: "Name can only contain letters and spaces.",
     }),
+
   email: z
     .string()
     .min(1, { message: "Email is required." })
     .email({ message: "Please provide a valid email address." }),
+
   password: z
     .string()
     .min(6, { message: "Password must be at least 6 characters long." })
@@ -43,6 +49,7 @@ export const SignUpSchema = z.object({
       message: "Password must contain at least one special character.",
     }),
 });
+
 export const AskQuestionSchema = z.object({
   title: z
     .string()
@@ -60,6 +67,7 @@ export const AskQuestionSchema = z.object({
     .min(1, { message: "At least one tag is required." })
     .max(3, { message: "Cannot add more than 3 tags." }),
 });
+
 export const UserSchema = z.object({
   name: z.string().min(1, { message: "Name is required." }),
   username: z
@@ -74,4 +82,29 @@ export const UserSchema = z.object({
     .url({ message: "Please provide a valid URL." })
     .optional(),
   reputation: z.number().optional(),
+});
+
+export const AccountSchema = z.object({
+  userId: z.string().min(1, { message: "User ID is required." }),
+  name: z.string().min(1, { message: "Name is required." }),
+  image: z.string().url({ message: "Please provide a valid URL." }).optional(),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long." })
+    .max(100, { message: "Password cannot exceed 100 characters." })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter.",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter.",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number." })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: "Password must contain at least one special character.",
+    })
+    .optional(),
+  provider: z.string().min(1, { message: "Provider is required." }),
+  providerAccountId: z
+    .string()
+    .min(1, { message: "Provider Account ID is required." }),
 });
