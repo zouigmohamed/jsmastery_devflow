dc73eab93e8eedf259d847cfec10cff999c95b27
zouigmohamed <zouig.mohamed@gmail.com>
2025-03-12 18:00:13 +0000

    implement get all users a

---
+++ lib/actions/user.action.ts
@@ -0,0 +1,74 @@
+"use server";
+
+import { FilterQuery } from "mongoose";
+
+import { User } from "@/database";
+
+import action from "../handlers/action";
+import handleError from "../handlers/error";
+import { PaginatedSearchParamsSchema } from "../validation";
+
+export async function getUsers(
+  params: PaginatedSearchParams
+): Promise<ActionResponse<{ users: User[]; isNext: boolean }>> {
+  const validationResult = await action({
+    params,
+    schema: PaginatedSearchParamsSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { page = 1, pageSize = 10, query, filter } = params;
+
+  const skip = (Number(page) - 1) * pageSize;
+  const limit = pageSize;
+
+  const filterQuery: FilterQuery<typeof User> = {};
+
+  if (query) {
+    filterQuery.$or = [
+      { name: { $regex: query, $options: "i" } },
+      { email: { $regex: query, $options: "i" } },
+    ];
+  }
+
+  let sortCriteria = {};
+
+  switch (filter) {
+    case "newest":
+      sortCriteria = { createdAt: -1 };
+      break;
+    case "oldest":
+      sortCriteria = { createdAt: 1 };
+      break;
+    case "popular":
+      sortCriteria = { reputation: -1 };
+      break;
+    default:
+      sortCriteria = { createdAt: -1 };
+      break;
+  }
+
+  try {
+    const totalUsers = await User.countDocuments(filterQuery);
+
+    const users = await User.find(filterQuery)
+      .sort(sortCriteria)
+      .skip(skip)
+      .limit(limit);
+
+    const isNext = totalUsers > skip + users.length;
+
+    return {
+      success: true,
+      data: {
+        users: JSON.parse(JSON.stringify(users)),
+        isNext,
+      },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
