28452685feafea075a63641c98b8cba27f87db7b
zouigmohamed <zouig.mohamed@gmail.com>
2025-05-04 21:02:31 +0100

    fix font

--- components/navigation/LeftSideBar.tsx
+++ components/navigation/LeftSideBar.tsx
@@ -12,6 +12,7 @@ import { Button } from "../ui/button";
 const LeftSidebar = async () => {
   const session = await auth();
   const userId = session?.user?.id;
+  console.log("first")
 
   return (
     <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
